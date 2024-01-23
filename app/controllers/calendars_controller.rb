86d637c7e02fd7cca5f51e6497e8c99a69053378
iroha1021 <150418497+iroha1021@users.noreply.github.com>
2024-01-23 14:05:59 +0900

    修正再

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,14 +2,8 @@ class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
-<<<<<<< HEAD
     get_week
-=======
-
-    get_Week
-
->>>>>>> b4b1371e3b1566b2c59099fccec1bb487409ced0
-    @plan = Plan.new
+   @plan = Plan.new
   end
 
   # 予定の保存
@@ -24,29 +18,15 @@ class CalendarsController < ApplicationController
     params.require(:plan).permit(:date, :plan)
   end
 
-<<<<<<< HEAD
+
   def get_week
-    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-=======
-
-  def get_Week
->>>>>>> b4b1371e3b1566b2c59099fccec1bb487409ced0
-
-
-
-
-      wday_num = Date.today.wday + x # wdayメソッドを用いて取得した数値
-      if wday_num >= 7 #「wday_numが7以上の場合」という条件式
-        wday_num = wday_num -7
-      end
-     
-
-<<<<<<< HEAD
+    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)','(日)'] 
+     wday_num = Date.today.wday + x # wdayメソッドを用いて取得した数値
+        if wday_num >= 7 #「wday_numが7以上の場合」という条件式
+         wday_num = wday_num -7
+       end
       days = { month:(@todays_date + x).month, data:(@todays_date + x).day, plan: today_plans, wday:wdays[wday_num]}
       @week_days.push(days)
     end
   end
 end  
-=======
-      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdays[wday_num]}
->>>>>>> b4b1371e3b1566b2c59099fccec1bb487409ced0
