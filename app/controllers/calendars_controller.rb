11e46b2ebac9b7869d4223be63e9ab85302003aa
iroha1021 <150418497+iroha1021@users.noreply.github.com>
2024-01-24 19:57:34 +0900

    修正3

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -20,13 +20,28 @@ class CalendarsController < ApplicationController
 
 
   def get_week
-    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)','(日)'] 
-     wday_num = Date.today.wday + x # wdayメソッドを用いて取得した数値
-        if wday_num >= 7 #「wday_numが7以上の場合」という条件式
-         wday_num = wday_num -7
-       end
-      days = { month:(@todays_date + x).month, data:(@todays_date + x).day, plan: today_plans, wday:wdays[wday_num]}
+    wdays = ['日','月','火','水','木','金','土']
+
+    @week_days = []
+    @todays_date = Date.today
+
+    plans = Plan.where(date: @todays_date..@todays_date + 6)
+
+    7.times do |x|
+      today_plans = []
+      plans.each do |plan|
+        today_plans.push(plan.plan) if plan.date == @todays_date + x
+      end
+
+      wday_num = Date.today.wday + x
+
+      if wday_num >= 7
+        wday_num = wday_num -7
+      end
+
+      days = {month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wday: wdays[wday_num]}
       @week_days.push(days)
+
     end
-  end
-end  
+ end  
+end
